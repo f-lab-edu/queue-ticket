f71fa5a8e0a00f0a588db33195a722f8441d9839
alrscent <mihyang.sol.lee@mgail.com>
2026-03-24 16:37:01 +0900

    #2 회원가입

--- src/main/java/com/queuetix/member/controller/MemberController.java
+++ src/main/java/com/queuetix/member/controller/MemberController.java
@@ -1,7 +1,18 @@
 package com.queuetix.member.controller;
 
+import com.queuetix.member.dto.MemberResponse;
+import com.queuetix.member.dto.MemberSignUpRequest;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberController {
+    public MemberController() {}
+
+    @PostMapping("/members")
+    public MemberResponse addMember(@RequestBody @Valid MemberSignUpRequest request) {
+        return null;
+    }
 }
